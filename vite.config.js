90184440df2f5d6afc173cdecc3c1b586bd2a14e
ankshra28 <2805ayumishra@gmail.com>
2026-05-29 09:35:06 -0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/NGO-profile/'
+  base:'/NGO-profile/',
+  base: process.env.VITE_BASE_PATH || "/NGO-vite-proflie"
 })
